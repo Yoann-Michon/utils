9c1a4399ce9b7e18717403f58dd2a5c7930dcf4e
unknown <yoann972.wm@outlook.com>
2025-04-03 20:41:30 +0200

    fix enum graphql

--- roles_guard/src/role.enum.ts
+++ roles_guard/src/role.enum.ts
@@ -1,4 +1,11 @@
+import { registerEnumType } from "@nestjs/graphql";
+
 export enum Role{
     STUDENT="STUDENT",
     PROFESSOR="PROFESSOR"
 }
+
+registerEnumType(Role, {
+    name: 'Role',
+    description: 'Rôles utilisateur disponibles',
+  });
